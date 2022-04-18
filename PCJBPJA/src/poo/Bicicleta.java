e43534642f957b292c6212ea0f27a559280589d5
Carlos <Carlos@DESKTOP-F3GM3EK>
2022-04-17 21:14:31 -0300

    Add todos os demais projetos...

---
+++ PCJBPJA/src/poo/Bicicleta.java
@@ -0,0 +1,16 @@
+package poo;
+
+public class Bicicleta {
+String cor;
+String modelo;
+int ano;
+
+void andar() {
+	System.out.println("Pedalando para andar!");
+}
+
+void frear() {
+	System.out.println("Freando para parar!");
+}
+
+}
