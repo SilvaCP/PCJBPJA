a4259bf4beb6623549bdf2f19051e67f8394e57b
SilvaCP <79268743+SilvaCP@users.noreply.github.com>
2022-04-17 21:58:17 -0300

    Upadete do código
    Foi acrescentado algumas linhas.

--- PCJBPJA/src/poo/BMW.java
+++ PCJBPJA/src/poo/BMW.java
@@ -18,8 +18,20 @@ public static void main(String[] args) {
 	bmw.desligar();
 	System.out.println("Motor da BMW desligado!");
 	
+	Carro mustang = new Carro();
+	mustang.ano = 2023;
+	mustang.cor = "Azul";
 	
+	System.out.println("Carro: Mustang");
+	System.out.println("Ano: " + mustang.ano);
+	System.out.println("Cor: " + mustang.cor);
 	
+	mustang.ligar();
+	System.out.println("Mustang ligado!");
+	mustang.acelerar();
+	System.out.println("VRUUUUUUuuuuuu.");
+	mustang.desligar();
+	System.out.println("Motor do Mustang desligado!");
 	
 }
 }
