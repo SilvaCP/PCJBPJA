3899f015a6e50e636e265ed07698e0c6cc6ace0d
SilvaCP <79268743+SilvaCP@users.noreply.github.com>
2022-04-22 21:49:53 -0300

    Update do código
    Fiz alteração no código! Acrescentei  na classe carro o tipo Fusca!

--- PCJBPJA/src/poo/BMW.java
+++ PCJBPJA/src/poo/BMW.java
@@ -33,5 +33,21 @@ public static void main(String[] args) {
 	mustang.desligar();
 	System.out.println("Motor do Mustang desligado!");
 	
+	Carro fusca = new Carro();
+	fusca.ano = 1978;
+	fusca.cor = "Branco";
+	
+	System.out.println("Carro: Fusca");
+	System.out.println("Ano: " + fusca.ano);
+	System.out.println("Ano: " + fusca.cor);
+	
+	fusca.ligar();
+	System.out.println("Fusca ligado!");
+	fusca.acelerar();
+	System.out.println("Vruuuuuuuu!");
+	fusca.desligar();
+	System.out.println("Motor do Fusca desligado!");
+	
+	
 }
 }
